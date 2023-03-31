f06fc6bc5afd8d793891ea50edb4935e255c3ebd
Mohammed Mudassir ullah Khan <mudassir222001@gmail.com>
2023-03-31 16:33:56 +0530

    Commit message

--- src/weatherservice.js
+++ src/weatherservice.js
@@ -1,9 +1,9 @@
 const API_KEY='dfb7377eaa0531383d6c4ccd5e810b5f'
 import axios from "axios"
 const getLatLon = async(city)=>{
-    const URL=`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
+    const URL=`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`
     const res = await axios.get(URL);
-    // console.log(res?.data)
+    console.log(res?.data)
          return res?.data
 
 }
@@ -11,7 +11,7 @@ const getLatLon = async(city)=>{
 const getFormattedWeatherData =async(lat,lon,units)=>{
     const URL=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=${units}`
     const res = await axios.get(URL);
-    // console.log(res?.data)
+    console.log(res?.data)
          return res?.data
 }
 export {getLatLon,getFormattedWeatherData}  
